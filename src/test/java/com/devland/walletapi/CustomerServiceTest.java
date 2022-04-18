bf2d11be0124c3371004d113b781f31a7c432f7c
haristislami <islamiharist1998@gmail.com>
2022-04-18 13:33:57 +0700

    Add testing

--- src/test/java/com/devland/walletapi/CustomerServiceTest.java
+++ src/test/java/com/devland/walletapi/CustomerServiceTest.java
@@ -1,4 +1,28 @@
 package com.devland.walletapi;
 
-public class CustomerServieTest {
+import com.devland.walletapi.customer.Customer;
+import com.devland.walletapi.customer.CustomerRepository;
+import com.devland.walletapi.customer.CustomerService;
+import com.devland.walletapi.transaction.TransactionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class CustomerServiceTest {
+    @Autowired
+    private CustomerService customerService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+    private TransactionRepository transactionRepository;
+
+    @Test
+    void getAll_shouldCreateTransferOfCustomer_whenInvoked(){
+        LocalDateTime dateTime = LocalDateTime.of(2022, 04,17,07,07,07);
+
+    }
 }
--- src/test/java/com/devland/walletapi/CustomerTest.java
+++ src/test/java/com/devland/walletapi/CustomerTest.java
@@ -1,5 +1,36 @@
 package com.devland.walletapi;
 
-public class CustomerTest {
+import com.devland.walletapi.customer.Customer;
+import com.devland.walletapi.customer.CustomerResponseDTO;
+import com.devland.walletapi.wallet.Wallet;
+import com.devland.walletapi.wallet.WalletResponseDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+class CustomerTest {
     @Test
+            void convertToResponse_shouldReturnCustomerResponseDTO_whenInvoked(){
+        LocalDateTime dateTime = LocalDateTime.of(2022, 4, 18, 7, 7, 7);
+        LocalDateTime dob = LocalDateTime.of(1998, 12, 5, 7, 7, 7);
+
+        Customer customer = Customer.builder().id(1L).name("Harist").NIK("123456")
+                .dateOfBirth(dob)
+                .customerCreatedAt(dateTime)
+                .wallet(Wallet.builder().id(1L).walletBalance(0).transactions(null)
+                        .walletCreatedAt(dateTime).build())
+                .build();
+        CustomerResponseDTO expectedResult = CustomerResponseDTO.builder()
+                .name("Harist").NIK("123456")
+                .dateOfBirth(dob)
+                .customerCreatedAt(dateTime)
+                .walletResponseDTO(WalletResponseDTO.builder().walletBalance(0)
+                        .transactions(null).walletCreatedAt(dateTime).build()).build();
+
+        CustomerResponseDTO actualResult = customer.convertToResponse();
+
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
 }
